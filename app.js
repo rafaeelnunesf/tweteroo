fcb1861bc05fad69ae1299bb13b07aed8b128eec
rafaeelnunes <rafaelnfsq@gmail.com>
2022-01-22 13:50:30 -0300

    POST and GET request in the tweets route

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 import express, { json } from "express";
+import cors from "cors";
 
 const app = express();
 app.use(json());
+app.use(cors());
 
-const users = [];
+const users = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+
+const tweets = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+];
 
 app.post("/sign-up", (req, res) => {
   users.push(req.body);
   res.send("OK");
+});
+app.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  tweet.avatar = users.find(
+    (user) => user.username === req.body.username
+  ).avatar;
+  tweets.push(tweet);
+  res.send(tweet);
+});
+
+app.get("/tweets", (req, res) => {
+  let lastTweets;
+  tweets.length > 10 ? (lastTweets = tweets.slice(-10)) : (lastTweets = tweets);
+  res.send(lastTweets);
 });
 
 app.listen(5000, () => {
